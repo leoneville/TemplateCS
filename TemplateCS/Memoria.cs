6a50e51ab753d586244c30ce502ea06e54dc73ff
Leonardo Neville Barbosa Guimaraes <leonardo.guimaraes@fortbrasil.com.br>
2022-03-22 11:30:50 -0300

    first commit

---
+++ TemplateCS/Memoria.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace Memoria
+{
+    public class Mem
+    {
+        #region Importando pinvokes
+
+        [DllImport("kernel32.dll")]
+        static extern bool ReadProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, [Out] byte[] lpBuffer, int dwSize, IntPtr lpNumberOfBytesRead);
+
+        [DllImport("kernel32.dll")]
+        static extern bool WriteProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, byte[] lpBuffer, int dwSize, IntPtr lpNumberOfBytesWritten);
+
+        #endregion
+
+        #region Encontrando processo
+
+        public Process proc;
+
+        public Process GetProcess(string procname)
+        {
+            proc = Process.GetProcessesByName(procname)[0];
+            return proc;
+        }
+
+        public IntPtr GetModuleBase(string modulename)
+        {
+            if (modulename.Contains(".exe"))
+                return proc.MainModule.BaseAddress;
+
+            foreach (ProcessModule module in proc.Modules)
+            {
+                if (module.ModuleName == modulename)
+                    return module.BaseAddress;
+            }
+            return IntPtr.Zero;
+        }
+
+        #endregion
+
+        #region Lendo Memoria
+
+        public IntPtr RPM(IntPtr baseAddress)
+        {
+            byte[] buffer = new byte[4];
+            ReadProcessMemory(proc.Handle, baseAddress, buffer, buffer.Length, IntPtr.Zero);
+
+            return new IntPtr(BitConverter.ToInt32(buffer, 0));
+        }
+
+        public IntPtr RPM(IntPtr baseAddress, int offset)
+        {
+            byte[] buffer = new byte[4];
+            ReadProcessMemory(proc.Handle, IntPtr.Add(baseAddress, offset), buffer, buffer.Length, IntPtr.Zero);
+
+            return new IntPtr(BitConverter.ToInt32(buffer, 0));
+        }
+
+        #endregion
+
+        #region Lendo Bytes
+        public byte[] ReadBytes(IntPtr baseAddress, int bytes)
+        {
+            byte[] buffer = new byte[bytes];
+            ReadProcessMemory(proc.Handle, baseAddress, buffer, buffer.Length, IntPtr.Zero);
+
+            return buffer;
+        }
+
+        public byte[] ReadBytes(IntPtr baseAddress, int offset, int bytes)
+        {
+            byte[] buffer = new byte[bytes];
+            ReadProcessMemory(proc.Handle, IntPtr.Add(baseAddress, offset), buffer, buffer.Length, IntPtr.Zero);
+
+            return buffer;
+        }
+        #endregion
+
+        #region Escrevendo na Memoria
+        public bool WPM(IntPtr address, byte[] newbytes)
+        {
+            return WriteProcessMemory(proc.Handle, address, newbytes, newbytes.Length, IntPtr.Zero);
+        }
+
+        public bool WPM(IntPtr address, int offset, byte[] newbytes)
+        {
+            return WriteProcessMemory(proc.Handle, IntPtr.Add(address, offset), newbytes, newbytes.Length, IntPtr.Zero);
+        }
+        #endregion
+    }
+}
---
+++ TemplateCS/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Memoria;
+
+namespace TemplateCS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mem m = new Mem();
+
+            m.GetProcess("csgo"); // Identificando processo do jogo
+
+            var client = m.GetModuleBase("client.dll"); // Obtendo endereço base
+
+            int localPlayer = 0xDB35DC;
+            int health = 0x100;
+
+            var buffer = m.RPM(client, localPlayer);
+
+            while (true)
+            {
+                var hp = BitConverter.ToInt32(m.ReadBytes(buffer, health, 4), 0);
+
+                Console.WriteLine("Vida do jogador --> " + hp);
+            }
+        }
+    }
+}
